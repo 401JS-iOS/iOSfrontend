0150f8af7be5b59ea00cbf6126ad370225e23bd4
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-05-15 11:18:58 -0700

    Add gitignore

---
+++ help-me/help-me/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  help-me
+//
+//  Created by Robert Hatfield on 5/15/17.
+//  Copyright © 2017 Robert Hatfield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
