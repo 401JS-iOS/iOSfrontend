a2bd0e4b30c682b995b3e9a91c30c55d04f0527c
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-05-15 15:40:48 -0700

    Add Developer class

---
+++ dev-org/dev-org/Developer.h
@@ -0,0 +1,17 @@
+//
+//  Developer.h
+//  dev-org
+//
+//  Created by Robert Hatfield on 5/15/17.
+//  Copyright © 2017 Robert Hatfield. All rights reserved.
+//
+
+#import "User.h"
+
+@interface Developer : User
+
+@property (strong, nonatomic) NSArray *languages;
+@property (strong, nonatomic) NSArray *services;
+@property (nonatomic) Boolean isAvailable;
+
+@end
