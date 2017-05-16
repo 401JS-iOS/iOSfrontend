688e8f4cdaf9301cc02b662a261fa8356d8e5899
Rio <jayb967@gmail.com>
2017-05-15 17:27:13 -0700

    Added view user and NPO view controllers

---
+++ dev-org/dev-org/DevRequestsTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  DevRequestsTableViewCell.h
+//  dev-org
+//
+//  Created by Rio Balderas on 5/15/17.
+//  Copyright © 2017 Robert Hatfield. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DevRequestsTableViewCell : UITableViewCell
+
+@end
---
+++ dev-org/dev-org/DevRequestsTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  DevRequestsTableViewCell.m
+//  dev-org
+//
+//  Created by Rio Balderas on 5/15/17.
+//  Copyright © 2017 Robert Hatfield. All rights reserved.
+//
+
+#import "DevRequestsTableViewCell.h"
+
+@implementation DevRequestsTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
