fa09a0823e3c829aadcf60d792bee8d5f70142f1
Rio <jayb967@gmail.com>
2017-05-16 21:20:24 -0700

    registered nib file, created orgViewController

--- dev-org/dev-org/NPORequestsTableViewCell.m
+++ dev-org/dev-org/NPORequestsTableViewCell.m
@@ -5,10 +5,9 @@
 //  Created by Rio Balderas on 5/15/17.
 //  Copyright © 2017 Robert Hatfield. All rights reserved.
 //
+#import "NPORequestsTableViewCell.h"
 
-#import "DevRequestsTableViewCell.h"
-
-@implementation DevRequestsTableViewCell
+@implementation NPORequestsTableViewCell
 
 - (void)awakeFromNib {
     [super awakeFromNib];
