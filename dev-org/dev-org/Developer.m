ed43df277044e599ad52afc621edcda032687802
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-05-17 12:48:57 -0700

    Add NSCoding to User classes

--- dev-org/dev-org/Developer.m
+++ dev-org/dev-org/Developer.m
@@ -8,6 +8,35 @@
 
 #import "Developer.h"
 
+@interface Developer () <NSCoding>
+
+@end
+
 @implementation Developer
+
+#pragma mark - NSCoding
+- (void)encodeWithCoder:(NSCoder *)coder {
+    //    [super encodeWithCoder];
+    [coder encodeObject:_languages forKey:@"languages"];
+    [coder encodeObject:_services forKey:@"services"];
+    [coder encodeBool:_isAvailable forKey:@"isAvailable"];
+    [coder encodeObject:_radius forKey:@"radius"];
+    [coder encodeObject:_projects forKey:@"projects"];
+}
+
+- (instancetype)initWithCoder:(NSCoder *)decoder {
+    self = [super init];
+    if (!self) {
+        return nil;
+    }
+    
+    _languages = [decoder valueForKey:@"languages"];
+    _services = [decoder valueForKey:@"services"];
+    _isAvailable = [decoder decodeBoolForKey:@"isAvailable"];
+    _radius = [decoder valueForKey:@"radius"];
+    _projects = [decoder valueForKey:@"projects"];
+    
+    return self;
+}
 
 @end
--- dev-org/dev-org/User.m
+++ dev-org/dev-org/User.m
@@ -8,6 +8,42 @@
 
 #import "User.h"
 
+@interface User () <NSCoding>
+
+@end
+
 @implementation User
+
+#pragma mark - NSCoding
+- (void)encodeWithCoder:(NSCoder *)coder {
+    [coder encodeObject:_userID forKey:@"userID"];
+    [coder encodeObject:_username forKey:@"username"];
+    [coder encodeObject:_email forKey:@"email"];
+    [coder encodeObject:_address forKey:@"address"];
+    [coder encodeObject:_city forKey:@"city"];
+    [coder encodeObject:_state forKey:@"state"];
+    [coder encodeObject:_phone forKey:@"phone"];
+    [coder encodeObject:_profilePic forKey:@"profilePic"];
+    [coder encodeObject:_websites forKey:@"websites"];
+}
+
+- (instancetype)initWithCoder:(NSCoder *)decoder {
+    self = [super init];
+    if (!self) {
+        return nil;
+    }
+    
+    _userID = [decoder decodeObjectForKey:@"userID"];
+    _username = [decoder decodeObjectForKey:@"username"];
+    _email = [decoder decodeObjectForKey:@"email"];
+    _address = [decoder decodeObjectForKey:@"address"];
+    _city = [decoder decodeObjectForKey:@"city"];
+    _state = [decoder decodeObjectForKey:@"state"];
+    _phone = [decoder decodeObjectForKey:@"phone"];
+    _profilePic = [decoder decodeObjectForKey:@"profilePic"];
+    _websites = [decoder decodeObjectForKey:@"websites"];
+
+    return self;
+}
 
 @end
